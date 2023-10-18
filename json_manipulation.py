d93f73a8b7f9836eb49bad75fe658146dea209ad
NadavChen <nadav2282@gmail.com>
2023-10-18 16:33:04 +0300

    creating python script - Looping through JSON

---
+++ json_manipulation.py
@@ -0,0 +1,11 @@
+import json
+
+# Looping trough JSON
+with open('states.json') as file:
+  data = json.load(file)
+
+for state in data['states']:
+  del state['area_codes']
+  
+with open('new_states.json', 'w') as file:
+  json.dump(data, file, indent=2)
